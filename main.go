d5eaca848a71071fdc000154a52ff43de5b2f1cc
rhys706 <rhysmccormack.business@gmail.com>
2025-02-09 04:29:42 -0500

    updated

--- main.go
+++ main.go
@@ -1,14 +1,108 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1) // Safely increments
+		next.ServeHTTP(w, r)      // Calls the next handler in the middleware chain
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load() // Safely reads the integer value
+	w.Header().Set("Content-Type", "text/html")
+	html := `<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>`
+	fmt.Fprintf(w, html, hits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0) // Resets the hit counter to 0
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Fileserver hit counter reset"))
+}
+
+func jsonHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		// these tags indicate how the keys in the JSON should be mapped to the struct fields
+		// the struct fields must be exported (start with a capital letter) if you want them parsed
+		Body string `json:"body"`
+	}
+
+	type goodReturnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Valid bool `json:"valid"`
+	}
+
+	type badReturnVals struct {
+		// the key will be the name of struct field unless you give it an explicit JSON tag
+		Error string `json:"error`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respBody := badReturnVals{
+			Error: "Something went wrong",
+		}
+		dat, _ := json.Marshal(respBody)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write(dat)
+		return
+	}
+
+	if n := len(params.Body); n > 140 {
+		respBody := badReturnVals{
+			Error: "Chirp is too long",
+		}
+		dat, _ := json.Marshal(respBody)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(dat)
+		return
+	}
+	respBody := goodReturnVals{
+		Valid: true,
+	}
+	dat, _ := json.Marshal(respBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(dat)
+	return
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.Handle("GET /admin/metrics", http.HandlerFunc(apiCfg.metricsHandler))
+	mux.Handle("POST /admin/reset", http.HandlerFunc(apiCfg.resetHandler))
+	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(jsonHandler))
 
 	server := http.Server{
 		Addr:    ":8080",
